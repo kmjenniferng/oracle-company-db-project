4271d02f320117625f5be4f74fcd59b45eea2fd9
kmjenniferng <ka.man.jennifer.ng@gmail.com>
2021-11-05 17:05:33 -0400

    Updated a sql file for creating report
    Updated a sql file for creating report

--- create_yearly_report_sum_total_sales_employee.sql
+++ create_yearly_report_sum_total_sales_employee.sql
@@ -1,7 +1,7 @@
 -- Create yearly report to show sum of the total sales for each employee
 
 select emp.id as "EMPLOYEE_ID",
-emp.first_name, emp.last_name, emp.birth_date, emp.sex, 
+emp.first_name, emp.last_name,
 branch.name as "BRANCH_NAME",
 manager.first_name || ' ' || manager.last_name as "MANAGER_NAME",
 emp_total_sales.total_sales
